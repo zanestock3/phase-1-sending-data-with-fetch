660d052af957a29120c50d1a7d996e588f26ac3d
Zane Stock <zanestock3@gmail.com>
2022-04-28 07:36:29 -0500

    commit tree ab9a02ad6e9fc2dd77a58394366d225bcb040457

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const body = document.getElementsByTagName("body")[0];
+
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch ("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(function(error){
+        alert("Unauthorized Access");
+    })
+}
+submitData("Name", "name@name.com");
